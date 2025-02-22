2df5e51865ef4b1b4554d63201432c8478aad98a
meronmulu <meronmulu2121@gmail.com>
2025-02-23 01:43:35 +0300

    s

--- Backend/src/api/user/user.schema.js
+++ Backend/src/api/user/user.schema.js
@@ -14,7 +14,7 @@ const userSchema = {
     update: z.object({
         name: z.string().min(1, "Name is required"),
         email: z.string().email("Invalid email format"),
-        password: z.string().min(6, "Password must be at least 6 characters long"),
+        password: z.string().optional(),
         role: z.enum(["ADMIN", "MANAGER", "EMPLOYEE", "CLIENT"]).optional(),
     }),
 };
