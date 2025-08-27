cc393641834bc5c44baad49db0e10a8879eb488c
meronmulu <meronmulu2121@gmail.com>
2025-08-27 14:57:23 +0300

    final commit

--- Backend/prisma/migrations/20250629123455_add_cascade_on_task/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropForeignKey
-ALTER TABLE `task` DROP FOREIGN KEY `Task_projectId_fkey`;
-
--- DropIndex
-DROP INDEX `Task_projectId_fkey` ON `task`;
-
--- AddForeignKey
-ALTER TABLE `Task` ADD CONSTRAINT `Task_projectId_fkey` FOREIGN KEY (`projectId`) REFERENCES `Project`(`project_id`) ON DELETE CASCADE ON UPDATE CASCADE;
--- Backend/src/api/dashboard/dashboard.routes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getManagerDashboardSummary } = require("../dashboard/dashboard.controller");
-
-router.get("/manager-summary", getManagerDashboardSummary);
-
-module.exports = router;
